d99a494e56142bc1294e0885001903d2b6039a72
Anders Lundholm <anders.andlu.se>
2020-11-10 20:52:19 +0100

    Created new secret and added to gitignore.

--- todo_backend/todo_backend/settings.py
+++ todo_backend/todo_backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from corsheaders.defaults import default_headers
+from secrets import secret_key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y_#59w=(h7z#2qi6e1z(nwd*c$&+ny9%9e%1(@)1_vx$kols8@'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
